7e1a21e066d35c488fb6e5c0ba4ded436ecafd9e
Aqite93 <Aqite93@gmail.com>
2020-05-15 00:22:47 +0900

    implementation of news service

--- app/src/main/java/com/websarva/wings/android/anyinfo/NewsResponse.kt
+++ app/src/main/java/com/websarva/wings/android/anyinfo/NewsResponse.kt
@@ -1,4 +1,41 @@
 package com.websarva.wings.android.anyinfo
 
+import com.google.gson.annotations.SerializedName
+import java.net.URL
+
 class NewsResponse {
+    @SerializedName("status")
+    var status: String? = null
+
+    @SerializedName("totalResults")
+    var totalResults: Int? = 0
+
+    @SerializedName("articles")
+    var articles: Array<Articles>? = null
+}
+
+class Articles {
+    @SerializedName("source")
+    var source: Source? = null
+    @SerializedName("author")
+    var author: String? = null
+    @SerializedName("title")
+    var title: String? = null
+    @SerializedName("description")
+    var description: String? = null
+    @SerializedName("url")
+    var url: URL? = null
+    @SerializedName("urlToImage")
+    var urlToImage: URL? = null
+    @SerializedName("publishedAt")
+    var publishedAt: String? = null
+    @SerializedName("content")
+    var content: String? = null
+}
+
+class Source {
+    @SerializedName("id")
+    var id: String? = null
+    @SerializedName("name")
+    var name: String? = null
 }
--- app/src/main/java/com/websarva/wings/android/anyinfo/NewsService.kt
+++ app/src/main/java/com/websarva/wings/android/anyinfo/NewsService.kt
@@ -1,4 +1,11 @@
 package com.websarva.wings.android.anyinfo
 
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
 interface NewsService {
+    @Headers("x-api-key: <news api-key>")
+    @GET("http://newsapi.org/v2/top-headlines")
+    fun getNewsHeadlines(@Query("country") country: String): retrofit2.Call<NewsResponse>
 }
