3e9a710a5b371e63ccc546ca4d29ad1a21a01d56
Aqite93 <Aqite93@gmail.com>
2020-05-16 23:21:12 +0900

    get news and showing them on list view, moving news site

--- app/src/main/java/com/websarva/wings/android/anyinfo/MainActivity.kt
+++ app/src/main/java/com/websarva/wings/android/anyinfo/MainActivity.kt
@@ -1,17 +1,21 @@
 package com.websarva.wings.android.anyinfo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.widget.ListView
+import android.widget.SimpleAdapter
+import androidx.appcompat.app.AppCompatActivity
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.net.URL
 
 class MainActivity : AppCompatActivity() {
+    private var newsResponse: NewsResponse? = null
+
     companion object {
         var BaseUrl = "http://newsapi.org/v2/top-headlines/"
         var country = "us"
@@ -22,35 +26,62 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val lvNews = findViewById<ListView>(R.id.lvNews)
-        val newsList: MutableList<MutableMap<String, URL>> = mutableListOf()
-        val news = getNewsHeadline()
 
-        // TODO set news api response on list view
+        // When click list item, moving to news site
+        lvNews.setOnItemClickListener { parent, view, position, id ->
+            val element = parent.getItemAtPosition(position) as MutableMap<String, String>
+            Log.i("url: ", element["url"])
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(element["url"] as String))
+            startActivity(browserIntent)
+        }
+        getNewsHeadline()
     }
 
-    fun getNewsHeadline() {
+    /**
+     * Get world news title and url by using news api.
+     * After getting news, set them on list view.
+     */
+    private fun getNewsHeadline() {
         val retrofit = Retrofit.Builder()
             .baseUrl(BaseUrl)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
         val service = retrofit.create(NewsService::class.java)
         val call = service.getNewsHeadlines(country)
-        var news: NewsResponse
+
+        // call news api
         call.enqueue(object : Callback<NewsResponse> {
             override fun onResponse(call: Call<NewsResponse>, response: Response<NewsResponse>) {
                 if (response.code() == 200) {
-                    val newsResponse = response.body()!!
-                    Log.i("newsResponse: ", newsResponse.totalResults.toString())
-                    news = newsResponse
+                    newsResponse = response.body() as NewsResponse
+                    Log.i("newsResponse: ", "get news response")
+
+                    // create mutable data
+                    val lvNews = findViewById<ListView>(R.id.lvNews)
+                    val newsList: MutableList<MutableMap<String, String?>> = mutableListOf()
+
+                    newsResponse!!.articles?.forEach {
+                        val news = mutableMapOf("title" to it.title, "url" to it.url)
+                        newsList.add(news)
+                    }
+
+                    // set list view adapter
+                    val from = arrayOf("title", "url")
+                    val to = intArrayOf(android.R.id.text1, android.R.id.text2)
+                    val adapter = SimpleAdapter(
+                        applicationContext,
+                        newsList,
+                        android.R.layout.simple_list_item_2,
+                        from,
+                        to
+                    )
+                    lvNews.adapter = adapter
                 }
             }
 
             override fun onFailure(call: Call<NewsResponse>, t: Throwable) {
-                Log.i("failure", "failed...")
                 Log.i("failure getting news: ", t.message)
             }
         })
-
-        return news
     }
 }
