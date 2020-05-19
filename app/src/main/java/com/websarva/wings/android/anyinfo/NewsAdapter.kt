827e85f9c7ea58e66fa8e24e239f5bd44a2571a5
Aqite93 <Aqite93@gmail.com>
2020-05-19 23:17:58 +0900

    add showing fixed image by using inflater

--- app/src/main/java/com/websarva/wings/android/anyinfo/NewsAdapter.kt
+++ app/src/main/java/com/websarva/wings/android/anyinfo/NewsAdapter.kt
@@ -8,33 +8,20 @@ import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.ImageView
 import android.widget.TextView
+import kotlinx.android.synthetic.main.news_list.view.*
 
 class NewsAdapter(var context: Context, var newsList: ArrayList<News>) : BaseAdapter() {
-    private class ViewHolder(view: View?) {
-        var newsImage: ImageView = view?.findViewById(R.id.news_image) as ImageView
-        var newsTitle: TextView = view?.findViewById(R.id.news_title) as TextView
-        var newsUrl: TextView = view?.findViewById(R.id.news_url) as TextView
-        var newsDescription: TextView = view?.findViewById(R.id.news_description) as TextView
-        var newsPublishedAt: TextView = view?.findViewById(R.id.news_published_at) as TextView
-    }
+    val layoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        var viewHolder: ViewHolder
-        var convertView = convertView
-        if (convertView == null) {
-            convertView = LayoutInflater.from(parent?.context).inflate(R.layout.news_list, null)
-            viewHolder = ViewHolder(convertView)
-            convertView.tag = viewHolder
-        } else {
-            viewHolder = convertView.tag as ViewHolder
-        }
-        viewHolder.newsImage.setImageResource(newsList[position].image!!)
-        viewHolder.newsTitle.text = newsList[position].title
-        viewHolder.newsUrl.text = newsList[position].url
-        viewHolder.newsDescription.text = newsList[position].description
-        viewHolder.newsPublishedAt.text = newsList[position].published_at
+        val view = layoutInflater.inflate(R.layout.news_list, parent, false)
+        view.news_image.setImageResource(newsList[position].image!!)
+        view.news_title.text = newsList[position].title
+        view.news_url.text = newsList[position].url
+        view.news_description.text = newsList[position].description
+        view.news_description.text = newsList[position].published_at
 
-        return convertView!!
+        return view
     }
 
     override fun getItem(position: Int): Any {
